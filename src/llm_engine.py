a8ada5aefcff987e48ddbce37b5bde0d98f1fda9
[Hadi] <[sabahihadi@gmail.com]>
2026-06-25 00:17:44 +0330

    Changing Gemini model type

--- src/llm_engine.py
+++ src/llm_engine.py
@@ -93,7 +93,7 @@ News:
 
         try:
             response = client.models.generate_content(
-                model="gemini-2.5-flash",
+                model="gemini-2.5-flash-lite",
                 contents=prompt,
                 config={
                     "temperature": 0.3,
